46f2df19aac053783f51571820e75d6ae0c6dd5c
Keila Lopez <keilalofra@gmail.com>
2023-02-26 14:26:30 -0800

    completed app, reducer, and quote form

--- src/features/quotes/QuoteForm.js
+++ src/features/quotes/QuoteForm.js
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import { addQuote } from "./quotesSlice";
+// import the useDispatch hook
+import { useDispatch } from "react-redux";
 
 function QuoteForm() {
   const [formData, setFormData] = useState({
     // set up a controlled form with internal state
     // look at the form to determine what keys need to go here
+    author: "",
+    content: "",
   });
+
+  // set up dispatch hook 
+  const dispatch = useDispatch();
 
   function handleChange(event) {
     // Handle Updating Component State
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
   }
 
   function handleSubmit(event) {
     // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
+    event.preventDefault();
+    dispatch(
+      // Create quote object from state
+      // Pass quote object to action creator (that we imported)
+      addQuote({
+        ...formData,
+        id: uuid(),
+        votes: 0,
+      })
+    );
     // Update component state to return to default state
+    setFormData({
+      author: "",
+      content: "",
+    });
   }
 
   return (
